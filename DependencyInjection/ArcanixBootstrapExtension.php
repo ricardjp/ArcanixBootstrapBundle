4996bd4144eabda7cb93da83169a0745dcf2c76d
Jean-Philippe Ricard <ricardjp@arcanix.com>
2013-03-24 22:22:20 -0400

    added licenses in file headers, updated the README.md

--- DependencyInjection/ArcanixBootstrapExtension.php
+++ DependencyInjection/ArcanixBootstrapExtension.php
@@ -1,18 +1,30 @@
 <?php
 
+/**
+ * Copyright (C) 2013 Jean-Philippe Ricard.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace Arcanix\BootstrapBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Parameter;
-
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
+ * @author Jean-Philippe Ricard <ricardjp@arcanix.com>
  */
 class ArcanixBootstrapExtension extends Extension {
 	
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 ArcanixBootstrapBundle
 ======================
 
-Symfony 2 bundle for Twitter Bootstrap
+Minimal configuration Symfony 2 bundle for Twitter Bootstrap
+
+This bundle is compatible with Symfony 2.1.*.
+
+This bundle is based on the MopaBootstrapBundle.
+
+Includes
+* Twitter Bootstrap version 2.3.1
+* jQuery via SonatajQueryBundle (https://github.com/sonata-project/SonatajQueryBundle)
+* Less assets are already configured on bundle loading using lessphp
+* Bootstrap icons are available out the box without any need for voodoo magic
+* Form extensions from the MopaBootstrapBundle
+
+Installation
+
+First add the dependency in your composer.json:
+```json
+"require": {
+    "arcanix/bootstrap-bundle": "dev-master"
+},
+"repositories": [
+    {
+        "type": "vcs",
+        "url": "git://github.com/ricardjp/ArcanixBootstrapBundle.git"
+    }
+]
+```
+
+You must also register the bundle in app/AppKernel.php:
+```php
+public function registerBundles() {
+    $bundles = array(
+        new Arcanix\BootstrapBundle\ArcanixBootstrapBundle(),
+    );
+}
+```
+
+You are good to go, simple as that.
