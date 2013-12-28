0f7984ba5c055e5f3a704ec9e3dec3face816fdd
Jean-Philippe Ricard <ricardjp@arcanix.com>
2013-12-28 16:23:30 -0500

    registering twig extension in config

--- DependencyInjection/ArcanixBootstrapExtension.php
+++ DependencyInjection/ArcanixBootstrapExtension.php
@@ -35,6 +35,9 @@ class ArcanixBootstrapExtension extends Extension {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('twig.xml');
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__."/../Resources/config"));
         
         if (isset($config['form'])) {
