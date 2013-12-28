3b10535171d9fc5cb02ece0978c003c7a8c8fbf9
Jean-Philippe Ricard <ricardjp@arcanix.com>
2013-12-28 16:59:29 -0500

    updated WidgetFormTypeExtension to include new mopa arguments

--- Form/Extension/WidgetFormTypeExtension.php
+++ Form/Extension/WidgetFormTypeExtension.php
@@ -1,25 +1,12 @@
 <?php
 
-/**
- * Copyright (c) 2011 Mohrenweiser & Partner, Philipp A. Mohrenweiser
- * - http://www.mohrenweiserpartner.de
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/*
+ * This file is part of the MopaBootstrapBundle.
+ *
+ * (c) Philipp A. Mohrenweiser <phiamo@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Arcanix\BootstrapBundle\Form\Extension;
@@ -28,67 +15,89 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\Exception\CreationException;
 
+/**
+ * Extension for Form Widget Bootstrap handling
+ *
+ * @author phiamo <phiamo@googlemail.com>
+ */
 class WidgetFormTypeExtension extends AbstractTypeExtension
 {
+    /**
+     * @var array
+     */
+    protected $options;
 
+    /**
+     * Constructor.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        if (!is_array($options['widget_addon'])) {
-            throw new CreationException("The 'widget_addon' option must be an array");
-        }
-        if (in_array('percent', $view->vars['block_prefixes'])) {
-            if ($options['widget_addon']['type'] === null) {
-                $options['widget_addon']['type'] = 'append';
-            }
-        }
-        if (in_array('money', $view->vars['block_prefixes'])) {
-            if ($options['widget_addon']['type'] === null) {
-                $options['widget_addon']['type'] = 'prepend';
-            }
-        }
-        if (((isset($options['widget_addon']['text']) && $options['widget_addon']['text'] !== null)
-                || (isset($options['widget_addon']['icon']) && $options['widget_addon']['icon'] !== null)) && $options['widget_addon']['type'] === null) {
-            throw new \Exception('You must provide a "type" for widget_addon');
+        if (in_array('percent', $view->vars['block_prefixes']) && null === $options['widget_addon_append']) {
+            $options['widget_addon_append'] = array();
         }
 
-        $view->vars['widget_control_group'] = $options['widget_control_group'];
-        $view->vars['widget_controls'] = $options['widget_controls'];
-        $view->vars['widget_addon'] = $options['widget_addon'];
+        if (in_array('money', $view->vars['block_prefixes']) && null === $options['widget_addon_prepend']) {
+            $options['widget_addon_prepend'] = array();
+        }
+
+        $view->vars['widget_form_control_class'] = $options['widget_form_control_class'];
+        $view->vars['widget_form_group'] = $options['widget_form_group'];
+        $view->vars['widget_addon_prepend'] = $options['widget_addon_prepend'];
+        $view->vars['widget_addon_append'] = $options['widget_addon_append'];
         $view->vars['widget_prefix'] = $options['widget_prefix'];
         $view->vars['widget_suffix'] = $options['widget_suffix'];
         $view->vars['widget_type'] = $options['widget_type'];
-        $view->vars['widget_control_group_attr'] = $options['widget_control_group_attr'];
-        $view->vars['widget_controls_attr'] = $options['widget_controls_attr'];
-
+        $view->vars['widget_items_attr'] = $options['widget_items_attr'];
+        $view->vars['widget_form_group_attr'] = $options['widget_form_group_attr'];
+        $view->vars['widget_checkbox_label'] = $options['widget_checkbox_label'];
     }
+
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(
-            array(
-                'widget_control_group' => true,
-                'widget_controls' => true,
-                'widget_addon' => array(
-                    'type' => null, //false: dont add anything, null: using presets, anything; prepend; append
-                    'icon' => null,
-                    'text' => null,
-                ),
-                'widget_prefix' => null,
-                'widget_suffix' => null,
-                'widget_type' => '',
-                'widget_control_group_attr' => array(),
-                'widget_controls_attr' => array(),
-            )
-        );
+        $resolver->setDefaults(array(
+            'widget_form_control_class' => 'form-control',
+            'widget_form_group' => true,
+            'widget_addon_prepend' => null,
+            'widget_addon_append' => null,
+            'widget_prefix' => null,
+            'widget_suffix' => null,
+            'widget_type' => '',
+            'widget_items_attr' => array(),
+            'widget_form_group_attr' => array(
+                'class' => 'form-group'
+            ),
+            'widget_checkbox_label' => $this->options['checkbox_label'],
+        ));
         $resolver->setAllowedValues(array(
-                'widget_type' => array(
-                    'inline',
-                    '',
-                )
+            'widget_type' => array(
+                'inline',
+                '',
+            ),
+            'widget_checkbox_label' => array(
+                'label',
+                'widget',
+                'both',
             )
-        );
+        ));
     }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getExtendedType()
     {
         return 'form';
